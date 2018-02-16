e4875e4e16f2357f105c2d126ebbabc20fd1323c
barry <barry@nnnn.com>
2018-02-16 11:09:53 -0600

    new

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!flask/bin/python
 from flask import Flask, jsonify
+import random
 
 app = Flask(__name__)
 
@@ -8,83 +9,67 @@ def index():
     return "Hello, World!"
 	
 
-tasks = [
-    {
-    "actions": [
-        {
-            "causes": [
-                {
-                    "shortDescription": "Started by an SCM change"
-                }
-            ]
-        },
-        {},
-        {},
-        {}
-    ],
-    "artifacts": [],
-    "building": False,
-    "description": None,
-    "duration": 80326,
-    "estimatedDuration": 68013,
-    "executor": None,
-    "fullDisplayName": "my project #149",
-    "id": "2013-06-14_14-31-06",
-    "keepLog": False,
-    "number": 149,
-    "result": "SUCCESS",
-    "timestamp": 1371234666000,
-    "url": "http://localhost:8080/job/my project/149/",
-    "builtOn": "",
-    "changeSet": {
-        "items": [
-            {
-                "affectedPaths": [
-                    "SearchViewController.m",
-                    "Sample.strings"
-                ],
-                "author": {
-                    "absoluteUrl": "http://localhost:8080/user/my user",
-                    "fullName": "My User"
-                },
-                "commitId": "9032",
-                "timestamp": 1371234304048,
-                "date": "2013-06-14T18:25:04.048031Z",
-                "msg": "Author:my_author Description: changes Id: B-186199 Reviewer:reviewer_name",
-                "paths": [
-                    {
-                        "editType": "edit",
-                        "file": "/branches/project_name/iOS/_MainLine/project_name/SearchViewController.m"
-                    },
-                                       ],
-                "revision": 9032,
-                "user": "user_name"
-            }
-        ],
-        "kind": "svn",
-        "revisions": [
-            {
-                "module": "repo_url",
-                "revision": 8953
-            },
-            {
-                "module": "repo_url",
-                "revision": 9032
-            }
-        ]
-    },
-    "culprits": [
-        {
-            "absoluteUrl": "http://localhost:8080/user/username",
-            "fullName": "username"
-        }
-    ]
-}
-]
+orchestrationSuccess = {
+			"status": "success",
+			"commitedBy": "Johns Snow",
+			"build": 12341,
+			"timestamp": None,
+			"pendingChanges": [
+				{
+					"commitBy": "Tyrion Lannister",
+					"changeId": 41231
+				},
+				{
+					"commitBy": "Robb Stark",
+					"changeId": 12311
+				}
+			]
+		}
+		
+orchestrationFail = {
+			"status": "fail",
+			"commitedBy": "Jaime Lannister",
+			"build": 12134,
+			"timestamp": None,
+			"pendingChanges": [
+				{
+					"commitBy": "Tyrion Lannister",
+					"changeId": 1221
+				},
+				{
+					"commitBy": "Viserys Targaryen",
+					"changeId": 31231
+				}
+			]
+		}
+		
+orchestrationRunning = {
+				"status": "running",
+				"commitedBy": "Theon Greyjoy",
+				"build": 4325,
+				"timestamp": None,
+				"pendingChanges": [
+					{
+						"commitBy": "Sansa Stark",
+						"changeId": 6544
+					},
+					{
+						"commitBy": "Khal Drogo",
+						"changeId": 758
+					}
+				]
+			}
+			
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@app.route('/api/orchestration', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+	rand = random.randint(1,3)
+	response = orchestrationSuccess;
+	if rand == 2:
+		response = orchestrationFail;
+	if rand == 3:
+		response = orchestrationRunning;
+	return jsonify(response);
 
 
 if __name__ == '__main__':
